b1671b02c143dfde1c09245e24ad225da6020fb4
Sean Madondo <seanmadondo@Seans-MBP.fritz.box>
2020-06-23 21:57:01 +1200

    Updates to layout and tabs

--- ResiTools/popup.js
+++ ResiTools/popup.js
@@ -1,3 +1,29 @@
+
+// TAB Control functionality, will hide and show tabs on button click
+
+function navigateTab(evt, tabName){
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+     // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+     // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+//============================================================================================
+
+//TWoS Calculator Functionality
 let calculateBtn = document.getElementById("calculateBtn");
 
 if (calculateBtn) {

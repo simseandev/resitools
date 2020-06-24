feaaa3daefdeb70574e8502a9e5986c27300e7b5
Simon Lorimer <bumblesimsim@gmail.com>
2020-06-24 16:34:57 +1200

    Tabs now cycle through

--- ResiTools/popup.js
+++ ResiTools/popup.js
@@ -1,48 +1,73 @@
-//Utils
+// ============================================================================================
+// ====================================== Utils ===============================================
+// ============================================================================================
+
+//these should really go in another file but we'll leave them here for now
 
 function isInteger(value) { // returns true if integer
     return /^\d+$/.test(value);
 }
 
-
-// TAB Control functionality, will hide and show tabs on button click
-
-function navigateTab(evt, tabName){
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+function copyText(elementId) { //copy elements value to clipboard
+    var text = document.getElementById(elementId).value;
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
 }
-//============================================================================================
 
-//TWoS Calculator Functionality
+// ============================================================================================
+// =================================== Tab Control ============================================
+// ============================================================================================
+
+let launcherBtn = document.getElementById("launcherBtn");
+if (launcherBtn) {
+    launcherBtn.addEventListener("click", function () {clickTab("launcherDiv")});
+}
+
+let templatesBtn = document.getElementById("templatesBtn");
+if (templatesBtn) {
+    templatesBtn.addEventListener("click", function () {clickTab("templatesDiv")});
+}
+
+let emailBtn = document.getElementById("emailsBtn");
+if (emailBtn) {
+    emailBtn.addEventListener("click", function () {clickTab("emailsDiv")});
+}
+
+let TWoSBtn = document.getElementById("TWoSBtn");
+if (TWoSBtn) {
+    TWoSBtn.addEventListener("click", function () {clickTab("TWoSDiv")});
+}
+
+function clickTab(elementId) {
+
+    document.getElementById("launcherDiv").style.display = "none";
+    document.getElementById("templatesDiv").style.display = "none";
+    document.getElementById("emailsDiv").style.display = "none";
+    document.getElementById("TWoSDiv").style.display = "none";
+
+    document.getElementById(elementId).style.display = "block";
+}
+
+// ============================================================================================
+// ========================= TWoS Calculator Functionality ====================================
+// ============================================================================================
+
 let calculateBtn = document.getElementById("calculateBtn");
 if (calculateBtn) {
     calculateBtn.addEventListener("click", calculateTimeWithoutService);
 }
 
-let copyAmount = document.getElementById("copyAmount");
-if (copyAmount) {
-    copyAmount.addEventListener("click", copyAmountToClipboard);
+let copyAmountBtn = document.getElementById("copyAmountBtn");
+if (copyAmountBtn) {
+    copyAmountBtn.addEventListener("click", function () {copyText("resultAmount")});
 }
 
-let copyString = document.getElementById("copyString");
-if (copyString) {
-    copyString.addEventListener("click", copyStringToClipboard);
+let copyStringBtn = document.getElementById("copyStringBtn");
+if (copyStringBtn) {
+    copyStringBtn.addEventListener("click", function () {copyText("resultString")});
 }
 
 function calculateTimeWithoutService() {
@@ -74,25 +99,5 @@ function calculateTimeWithoutService() {
     let reversalAmount = ((monthlyCosts * 12 / 365) * timeWithoutService).toFixed(2);
 
     document.getElementById("resultAmount").value = reversalAmount;
-
     document.getElementById("resultString").value = "Hi team. Please reverse $" + reversalAmount + " for " + timeWithoutService + " days of TWoS. Approved by " + approvedBy + ".";
 }
-
-function copyAmountToClipboard() {
-    var text = document.getElementById("resultAmount").value;
-    navigator.clipboard.writeText(text).then(function(){
-        console.log('Async: Copying to clipboard was successful!');
-    }, function(err) {
-        console.error('Async: Could not copy text: ', err);
-    });
-}
-
-function copyStringToClipboard() {
-    var text = document.getElementById("resultString").value;
-    navigator.clipboard.writeText(text).then(function(){
-        console.log('Async: Copying to clipboard was successful!');
-    }, function(err) {
-        console.error('Async: Could not copy text: ', err);
-    });
-}
-

262efbc7d7ac3ede91ae2946f3de5f58a93b0011
seanmadondo <seanmadondo@yahoo.com>
2020-12-01 22:08:12 +1300

    Boilerplate and some designs added to the Template tab

--- ResiTools/popup.js
+++ ResiTools/popup.js
@@ -22,10 +22,7 @@ let mySettingsDiv = document.getElementById("mySettingsDiv");
 if (mySettingsDiv) { mySettingsDiv.style.display = "none"; }
 
 let welcomeDiv = document.getElementById("welcomeDiv");
-if (welcomeDiv) {
-    getSettings();
-    welcomeDiv.style.display = "block"; 
-}
+if (welcomeDiv) { welcomeDiv.style.display = "block";}
 
 let aboutDiv = document.getElementById("aboutDiv");
     if (aboutDiv) { aboutDiv.style.display = "none"; }
@@ -34,40 +31,7 @@ let aboutDiv = document.getElementById("aboutDiv");
 //======================================= Utils ===============================================
 //=============================================================================================
 
-function getSettings() {
 
-    //firstName
-    chrome.storage.sync.get(["firstName"], function (result) {
-        if (result.firstName == undefined) {
-            document.getElementById("configOK").style.display = "none";
-            document.getElementById("configNOK").style.display = "block";
-            return;
-        }
-    });
-
-    //lastName
-    chrome.storage.sync.get(["lastName"], function (result) {
-        if (result.lastName == undefined) {
-            document.getElementById("configOK").style.display = "none";
-            document.getElementById("configNOK").style.display = "block";
-            return;
-        }
-    });
-
-    //workEmail
-    chrome.storage.sync.get(["workEmail"], function (result) {
-        if (result.workEmail == undefined) {
-            document.getElementById("configOK").style.display = "none";
-            document.getElementById("configNOK").style.display = "block";
-            return;
-        }
-    });
-    
-    document.getElementById("configOK").style.display = "block";
-    document.getElementById("configNOK").style.display = "none";
-}
-
-//these should really go in another file but we'll leave them here for now
 
 function isStringNumber(value) { //string value eg "8.42"
     return parseFloat(value.match(/^-?\d*(\.\d+)?$/))>0;
@@ -128,7 +92,7 @@ let templatesBtn = document.getElementById("templatesBtn");
 if (templatesBtn) {
     templatesBtn.addEventListener("click", function () {
         clickTab("templatesDiv");
-        loadTemplates();
+        // loadTemplates();
     });
 }
 
@@ -159,7 +123,7 @@ function clickTab(elementId) {
 
     document.getElementById("launcherDiv").style.display = "none";
     document.getElementById("templatesDiv").style.display = "none";
-    document.getElementById("emailsDiv").style.display = "none";
+    // document.getElementById("emailsDiv").style.display = "none";
     document.getElementById("TWoSDiv").style.display = "none";
     document.getElementById("welcomeDiv").style.display = "none";
     document.getElementById("mySettingsDiv").style.display = "none";
@@ -196,307 +160,13 @@ function loadSettings() {
 }
 
 //=============================================================================================
-//================================== Default Templates ========================================
+//===================================== Templates =============================================
 //=============================================================================================
 
-//check for selected
 
-//update template
-let updateTemplateBtn = document.getElementById("updateTemplateBtn");
-if (updateTemplateBtn) {
-    updateTemplateBtn.addEventListener("click", updateTemplate);
-}
-
-function updateTemplate () {
-    let confirmUpdate = confirm("Do you wish to update template?");
-    if (confirmUpdate == false) {
-        return;
-    }
-
-    //check title and desc are not empty
-    let updateName = document.getElementById("editTemplateName").value;
-    let updateDescription = document.getElementById("editTemplateDescription").value;
-
-    if (updateName === "") {
-        alert("Template name cannot be empty!");
-        return;
-    } else if (updateDescription === "") {
-        alert("Template description cannot be empty!");
-        return;
-    }
-
-    var templateId = document.getElementsByClassName("list-group-item active")[0].id;
-
-    chrome.storage.sync.get(["myTemplates"], function (result) {
-        if (result.myTemplates != undefined) {
-            console.log("Retrieved Storage from Chrome: myTemplates");
-            var myTemplates = result.myTemplates;
-            for (i = 0; i < myTemplates.length; i++) {
-                if (myTemplates[i][0] == templateId) {
-                    myTemplates.splice(i, 1);
-                    myTemplates.splice(i, 0, [updateName.replace(/\s+/g, '-'), updateDescription.split('\n')]);
-                    setStorage({"myTemplates": myTemplates});
-                    loadTemplates();
-                    $('#editTemplate').modal('hide');
-                    return;
-                }
-            }
-        }
-    });
-}
-
-//edit template, open modal with current information
-let editTemplateBtn = document.getElementById("editTemplateBtn");
-if (editTemplateBtn) {
-    editTemplateBtn.addEventListener("click", editTemplate);
-}
-
-function editTemplate() {
-    try {
-        var templateId = document.getElementsByClassName("list-group-item active")[0].id;
-        document.getElementById("editTemplateName").value = templateId.replace(/-/g, ' ');
-        $('#editTemplate').modal('show');
-        chrome.storage.sync.get(["myTemplates"], function (result) {
-            if (result.myTemplates != undefined) {
-                console.log("Retrieved Storage from Chrome: myTemplates");
-                var myTemplates = result.myTemplates;
-                for (i = 0; i < myTemplates.length; i++) {
-                    if (myTemplates[i][0] == templateId) {
-                        document.getElementById("editTemplateDescription").value = myTemplates[i][1].join('\n');
-                    }
-                }
-            }
-        });
-    } catch(err) {
-        console.log("editTemplate() Error: " + err);
-        return;
-    }
-}
-
-//delete button
-let deleteTemplateBtn = document.getElementById("deleteTemplateBtn");
-if (deleteTemplateBtn) {
-    deleteTemplateBtn.addEventListener("click", deleteTemplate);
-}
-
-function deleteTemplate() {
-    try {
-        var templateId = document.getElementsByClassName("list-group-item active")[0].id;
-        var confirmDeletion = confirm('Do you wish to delete template: "' + templateId.replace(/-/g, ' ') + '"');
-        if (confirmDeletion == true) {
-            //delete template with that id
-            chrome.storage.sync.get(["myTemplates"], function (result) {
-                if (result.myTemplates != undefined) {
-                    console.log("Retrieved Storage from Chrome: myTemplates");
-                    var myTemplates = result.myTemplates;
-                    for (i = 0; i < myTemplates.length; i++) {
-                        if (myTemplates[i][0] == templateId) {
-                            myTemplates.splice(i, 1);
-                            setStorage({"myTemplates": myTemplates});
-                            loadTemplates();
-                            return;
-                        }
-                    }
-                }
-            });
-        }
-        return;
-    } catch(err) {
-        console.log("deleteTemplate() Error: " + err);
-        return;
-    }
-}
-
-//add template
-let addTemplateBtn = document.getElementById("addTemplateBtn");
-if (addTemplateBtn) {
-    addTemplateBtn.addEventListener("click", addTemplate);
-}
-
-function addTemplate() {
-    let templateName = document.getElementById("templateName").value;
-    let templateDescription = document.getElementById("templateDescription").value;
-
-    //check not empty
-    if (templateName === "") {
-        alert("Template Name must not be empty!");
-        return;
-    } else if (templateDescription === "") {
-        alert("Template Description must not be empty!");
-        return;
-    } else if (checkSpecialChar(templateName)) {
-        alert("Template Name cannot contain special characters!")
-        return;
-    }
-
-    templateDescription = templateDescription.split('\n');
-
-    storeName = templateName.replace(/\s+/g, '-');
-
-    //store template isn sync
-    storeTemplate(storeName, templateDescription);
-
-}
-
-function storeTemplate(name, description) {
-    //check if templates exist already
-    chrome.storage.sync.get(["myTemplates"], function (result) {
-        if (result.myTemplates != undefined) { //if templates don't exist, then create
-            console.log("Retrieved Storage from Chrome: myTemplates");
-            var myTemplates = result.myTemplates;
-            for (i = 0; i < myTemplates.length; i++) { // check doesn't exist
-                if (myTemplates[i][0] === name) {
-                    alert("Template Name already exists");
-                    return;
-                }
-            }
-            myTemplates.push([name, description]);
-            setStorage({"myTemplates": myTemplates.sort()})
-            console.log("Template " + name + " has been saved");
-        } else { //if they exist, myTemplates += new template
-            setStorage({"myTemplates": [[name, description]]});
-            console.log("Template " + name + " has been saved");
-        }
-
-        //load new templates, close and reset modal
-        loadTemplates();
-        $('#addTemplate').modal('hide');
-        document.getElementById("templateName").value = "";
-        document.getElementById("templateDescription").value = "";
-    });
-}
-
-function loadTemplates() {
-    document.querySelector(".list-group").innerHTML = "";
-    document.querySelector(".tab-content").innerHTML = "";
-
-    document.querySelector(".tab-content").innerHTML = "<textarea id='templateBlank' rows='7' cols='50'></textarea>";
-    document.getElementById("templateBlank").disabled = true;
-
-    chrome.storage.sync.get(["myTemplates"], function (result) {
-        if (result.myTemplates != undefined) { 
-            console.log("Retrieved Storage from Chrome: myTemplates");
-
-            document.querySelector(".tab-content").innerHTML = "";
-
-            document.getElementById("editTemplateBtn").disabled = false;
-            document.getElementById("deleteTemplateBtn").disabled = false;
-            document.getElementById("copyTemplateBtn").disabled = false;
-
-            var myTemplates = result.myTemplates;
-
-            if (myTemplates.length == 0) {
-                document.querySelector(".tab-content").innerHTML = "<textarea id='templateBlank' rows='7' cols='50'></textarea>";
-                document.getElementById("templateBlank").disabled = true;
-                document.getElementById("copyTemplateBtn").disabled = true;
-                document.getElementById("editTemplateBtn").disabled = true;
-                document.getElementById("deleteTemplateBtn").disabled = true;
-            }
-
-            for (var i = 0; i < myTemplates.length; i++) {
-                if (i == 0) {
-                    document.querySelector(".list-group").innerHTML += "<a class='list-group-item py-2 active list-group-item-action' id='" + myTemplates[i][0] + "' data-toggle='list' href='#list-" + myTemplates[i][0] + "' role='tab' aria-controls='" + myTemplates[i][0] +"'>" + myTemplates[i][0].replace(/-/g, ' '); + "</a>";
-                    document.querySelector(".tab-content").innerHTML += "<textarea class='tab-pane show active' id='list-" + myTemplates[i][0] + "' role='tabpanel' aria-labelledby='list-" + myTemplates[i][0] +"-list' rows='7' cols='50'>"+ myTemplates[i][1].join('\n') + "</textarea>";
-                } else {
-                document.querySelector(".list-group").innerHTML += "<a class='list-group-item py-2 list-group-item-action' id='" + myTemplates[i][0] + "' data-toggle='list' href='#list-" + myTemplates[i][0] + "' role='tab' aria-controls='" + myTemplates[i][0] +"'>" + myTemplates[i][0].replace(/-/g, ' '); + "</a>";
-                document.querySelector(".tab-content").innerHTML += "<textarea class='tab-pane' id='list-" + myTemplates[i][0] + "' role='tabpanel' aria-labelledby='list-" + myTemplates[i][0] +"-list' rows='7' cols='50'>"+ myTemplates[i][1].join('\n') + "</textarea>";
-                }
-            }
-        }
-        return;
-      });
-}
-
-let copyTemplateBtn = document.getElementById("copyTemplateBtn");
-if (copyTemplateBtn) {
-    copyTemplateBtn.addEventListener("click", copyTemplate);
-}
-
-function copyTemplate() {
-    try {
-        var templateId = document.getElementsByClassName("list-group-item active")[0].id;
-        var description = document.getElementById("list-" + templateId).value;
-        copyTemplateText(description);
-    } catch(err) {
-        console.log("copyTemplate() Error: " + err);
-        return;
-    }
-}
 
 //=============================================================================================
-//============================== Internal Email Templates =====================================
-//=============================================================================================
-
-let emailExceptionBtn = document.getElementById("emailExceptionBtn");
-if (emailExceptionBtn) {
-    emailExceptionBtn.addEventListener("click", function () {promptEmail(
-        "WFM@2degrees.nz",
-        " ",
-        "Exception - " + new Date().toLocaleDateString(), 
-        "Hi team,%0D%0DI have been doing X from X:XX to " + new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true}) + ".%0D%0DPlease make an exception for this."
-    )});
-}
-
-let finishedLateBtn = document.getElementById("finishedLateBtn");
-if (finishedLateBtn) {
-    finishedLateBtn.addEventListener("click", function () {promptEmail(
-        "WFM@2degrees.nz", 
-        " ", 
-        "Finished Late - " + new Date().toLocaleDateString(), 
-        "Hi team,%0D%0DI have finished my shift at " + new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true}) + ".%0D%0DCould I please have this time made up elsewhere, or alternatively may I submit an overtime form.", 
-    )});
-}
-
-let technicalDifficultiesBtn = document.getElementById("technicalDifficultiesBtn");
-if (technicalDifficultiesBtn) {
-    technicalDifficultiesBtn.addEventListener("click", function () {promptEmail(
-        "WFM@2degrees.nz",
-        " ",
-        "Technical Difficulties - " + new Date().toLocaleDateString(),
-        "Hi Team,%0D%0DI have been experiencing technical difficulties from X to " + new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true}) + ".%0D%0DPlease make an exception for this."
-    )});
-}
-
-let emailLMNPBtn = document.getElementById("emailLMNPBtn");
-if (emailLMNPBtn) {
-    emailLMNPBtn.addEventListener("click", function () {promptEmail(
-        "lmnp@2degrees.nz",
-        " ",
-        "Port Number ASAP - *USER*/*CUSTNO.*", 
-        "Hi team,%0D%0DThe customer is now showing to be online.%0D%0DCould we please update the request to port their number over to ASAP.%0D%0DCustomer Details: X/X%0DPhone number: X"
-    )});
-}
-
-let modemReturnBtn = document.getElementById("modemReturnBtn");
-if (modemReturnBtn) {
-    modemReturnBtn.addEventListener("click", function () {promptEmail(
-        "nz.enquiries@brightstar.com",
-        "ResidentialHelpdesk@2degrees.nz",
-        "Confirm Modem Return - *B* NUMBER HERE*",
-        "Hi team,%0D%0DCould we please confirm that the modem has been returned:%0D%0D*B* NUMBER HERE*"
-    )});
-}
-
-//base email template, rather than populating many functions
-async function promptEmail(mailTo, cc, subject, body) {
-    var getFirstName;
-    chrome.storage.sync.get(["firstName"], function (result) {
-        if (result.firstName == undefined) {
-            getFirstName = "Kind regards";
-        } else {
-            console.log("name:" + result.firstName);
-            getFirstName = "Kind regards,%0D" + result.firstName;
-        }
-    });
-
-    await wait(10);
-
-    //replace /n with %0D%0 for new line
-    window.open('mailto:' + mailTo + '?cc=' + cc + '&subject=' + subject +'&body=' + body + "%0D%0D" + getFirstName);
-}
-
-//=============================================================================================
-//========================== Calculators Functionality ====================================
+//============================== Calculators Functionality ====================================
 //=============================================================================================
 
 // --------- Preset the two divs... ----------
